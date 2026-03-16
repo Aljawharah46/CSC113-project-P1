c196990673f5b98dcaeebf572004394ab01eab84
raghad1212 <raghadalistudent@gmail.com>
2026-03-16 21:00:37 +0300

    Create EntertainmentService.java

---
+++ EntertainmentService.java
@@ -0,0 +1,12 @@
+public class EntertainmentService extends Service {
+    private int hours;
+
+    public EntertainmentService(String serviceName, double basePrice, int hours) {
+        super(serviceName, basePrice);
+        this.hours = hours;
+    }
+
+    public double calculateCost() {
+        return basePrice + (hours * 150);
+    }
+}
