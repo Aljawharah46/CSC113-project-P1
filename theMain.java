c7ea489323e76130b1472a6923a93fde7c91d420
Khawla Alsiddig <khawlalsiddig@gmail.com>
2026-03-24 15:14:42 +0300

    Add files via upload

---
+++ theMain.java
@@ -0,0 +1,182 @@
+import java.util.*;
+class theMain {
+public static void main (String[]args){
+Scanner read = new Scanner (System.in);
+int choice;
+do {
+System.out.println ("-------Menu------");
+System.out.println ("1.Creat party");
+System.out.println ("2.Manage Guests");
+System.out.println ("3.Manage services");
+System.out.println ("4.Total cost");
+System.out.println ("5.Print summary");
+System.out.println ("6.Exit");
+System.out.println ("----------------");
+System.out.println ("Enter the choice you want.");
+
+choice= read.nextInt();
+boolean partyCreated =false;
+switch (choice){
+case 1 -> {
+System.out.println ("Enter party name.");
+String name = read.nextLine ();
+System.out.println ("Enter the date (dd/mm/yy)");
+String date = read.nextLine ();
+System.out.println ("Enter the party location");
+String location = read.nextLine ();
+System.out.println ("Enter number of guests");
+int guestsNum = read.nextInt ();
+System.out.println ("Enter number of services");
+int servicesNum = read.nextInt ();
+
+Party clientParty = new Party (name,date, location,guestsNum,servicesNum );
+
+System.out.println ("Party is created successfully!");
+partyCreated =true;
+}
+
+
+case 2 -> {
+if (partyCreated){
+System.out.println ("----Guests Managment------");
+System.out.println ("1.Add gusets");
+System.out.println ("2.Remove gusets");
+System.out.println ("--------------------------");
+int guestChoice= read.nextInt();
+
+if (guestChoice ==1){
+System.out.println ("Enter the guest name.");
+String guestName = read.nextLine();
+System.out.println ("Enter the guest phone number.");
+String guestPhoneNum = read.nextLine();
+System.out.println ("Enter the guest invetaion ID.");
+String guestID = read.nextLine();
+Guest newGuest = new Guest (guestName,guestPhoneNum,guestID);
+clientParty.addGuest(newGuest); 
+}
+
+else if (guestChoice==2){
+System.out.println ("Enter the guest name you want to remove");
+String GuestToRemove = read.nextLine();
+clientParty.removeGuest(GuestToRemove);
+}
+
+else
+System.out.println ("Invalid choice!");
+
+
+}
+else 
+System.out.println ("Create a party first!");
+
+}
+
+
+case 3-> {
+if (partyCreated){
+System.out.println ("----Services Managment------");
+System.out.println ("1.Add service");
+System.out.println ("2.Remove service");
+System.out.println ("--------------------------");
+int serviceChoice= read.nextInt();
+if (serviceChoice==1){
+
+System.out.println ("--- The type of services available-----");
+System.out.println ("1.Entertainment");
+System.out.println ("2.Venue");
+System.out.println ("3.Catering");
+System.out.println ("----------------------------------------");
+int serviceType = read.nextInt();
+switch( serviceType){
+case 1 -> {
+System.out.println ("Enter the service name.");
+String serviceName = read.nextLine();
+System.out.println ("Enter the service base price.");
+double basePrice = read.nextDouble();
+System.out.println ("Enter the number of hours");
+int hours = read.nextInt();
+EntertainmentService newEntService = new  EntertainmentService (serviceName, basePrice, hours);
+clientParty.addService(newEntService);
+} 
+case 2 -> {
+System.out.println ("Enter the service name.");
+String serviceName = read.nextLine();
+System.out.println ("Enter the service base price.");
+double basePrice = read.nextDouble();
+System.out.println ("Enter the capacity");
+int capacity = read.nextInt();
+VenueService newVenService = new  VenueService (serviceName, basePrice, capacity);
+clientParty.addService(newVanService);
+
+}
+case 3 -> {
+System.out.println ("Enter the service name.");
+String serviceName = read.nextLine();
+System.out.println ("Enter the service base price.");
+double basePrice = read.nextDouble();
+System.out.println ("Enter the number of guests");
+int cateringNumGuests = read.nextInt();
+CateringService newCatService = new  CateringService (serviceName, basePrice, cateringNumGuests);
+clientParty.addService(newCatService);}
+
+default ->System.out.println ("Invalid choice!");
+}
+
+}
+
+if else (serviceChoice==2){
+System.out.println ("Enter the service name you want to remove");
+String ServiceToRemove = read.nextLine();
+clientParty.removeService(ServiceToRemove);
+
+}
+
+else
+System.out.println ("Invalid choice!");
+
+}
+
+else 
+System.out.println ("Error! Create a party first!");
+}
+
+
+
+case 4->{
+if (partyCreated){
+double totalCost = clientParty.calculateTotalCost();
+System.out.println ("The total cost of the party is " + totalCost + ".");
+}
+else
+System.out.println ("Error! Create a party first!");
+}
+
+
+case 5-> {
+if (partyCreated)
+ clientParty.displayPartyDetails();
+else
+System.out.println ("Error! Create a party first!");
+
+}
+case 6-> {System.out.println ("It was nice having you with us.");
+}
+default ->{System.out.println ("Invaild choice! please choose again. ");
+}
+
+
+
+
+
+
+}
+
+
+
+
+}
+while (choice !=6);
+}
+
+
+}
