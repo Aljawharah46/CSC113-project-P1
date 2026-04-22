bd70532a0abdd0c896022c2afbff25e2c59de813
Aljawharah46 <aljbk4606@gmail.com>
2026-04-22 13:52:45 +0300

    Add files via upload

--- CateringService.java
+++ CateringService.java
@@ -2,12 +2,12 @@
 public class CateringService extends Service {
     private int numberOfGuests;
 
-    public CateringService(String serviceName, double basePrice, int numberOfGuests) {
+    public CateringService(String serviceName, double basePrice, int numberOfGuests) { //constructor
         super(serviceName, basePrice);
         this.numberOfGuests = numberOfGuests;
     }
 
-    public double calculateCost() {
+    public double calculateCost() { //calculates and returns the total cost of the catering service based on the number of guests
         return basePrice + (numberOfGuests * 25);
     }
     
