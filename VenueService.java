f3c346e8b8cd7d040cf7a2a61ebc88934b5bb900
Aljawharah46 <aljbk4606@gmail.com>
2026-04-22 13:58:01 +0300

    Add files via upload

--- VenueService.java
+++ VenueService.java
@@ -1,12 +1,13 @@
 public class VenueService extends Service {
     private int capacity;
 
-    public VenueService(String serviceName, double basePrice, int capacity) {
+    public VenueService(String serviceName, double basePrice, int capacity) { //constructor
         super(serviceName, basePrice);
         this.capacity = capacity;
     }
 
-    public double calculateCost() {
+    public double calculateCost() { /*calculates and returns the total cost of the venue service based on 
+its base price*/
         return basePrice + (capacity * 10);
     }
 }
