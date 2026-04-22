3719e81cf451ad1aae1a04c377aea574f57316da
Aljawharah46 <aljbk4606@gmail.com>
2026-04-22 13:55:32 +0300

    Add files via upload

--- Service.java
+++ Service.java
@@ -1,19 +1,19 @@
-public abstract class Service {
+public abstract class Service implements Payable {
     protected String serviceName;
     protected double basePrice;
 
-    public Service(String serviceName, double basePrice) {
+    public Service(String serviceName, double basePrice) { //constructor
         this.serviceName = serviceName;
         this.basePrice = basePrice;
     }
 
-    public String getServiceName() {
+    public String getServiceName() { //returns the service's name
         return serviceName;
     }
 
-    public abstract double calculateCost();
+    public abstract double calculateCost(); //calculates and returns the total cost of the service
 
-    public void displayService() {
+    public void displayService() { //displays the service's details
         System.out.println("Service Name: " + serviceName);
         System.out.println("Base Price: " + basePrice);
         System.out.println("Total Cost: " + calculateCost());
