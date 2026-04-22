dbc50f3286b47da4f2d729780057933263eea264
Aljawharah46 <aljbk4606@gmail.com>
2026-04-22 13:53:30 +0300

    Add files via upload

--- EntertainmentService.java
+++ EntertainmentService.java
@@ -2,12 +2,13 @@
 public class EntertainmentService extends Service {
     private int hours;
 
-    public EntertainmentService(String serviceName, double basePrice, int hours) {
+    public EntertainmentService(String serviceName, double basePrice, int hours) { //constructor
         super(serviceName, basePrice);
         this.hours = hours;
     }
 
-    public double calculateCost() {
+    public double calculateCost() { /*calculates and returns the total cost of the entertainment service 
+based on the number of hours*/
         return basePrice + (hours * 150);
     }
 }
