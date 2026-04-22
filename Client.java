39c2fb85e9a8509c9f703df87cae70bcf16c6ada
Aljawharah46 <aljbk4606@gmail.com>
2026-04-22 13:53:08 +0300

    Add files via upload

--- Client.java
+++ Client.java
@@ -1,24 +1,29 @@
-public class Client extends Person{
+public class Client extends Person implements Payable {
 
 private Party party=null;
 
-public Client(String n , String phoneNum){
+public Client(String n , String phoneNum){ //constructor
 super(n , phoneNum);
 }
 
-public void createParty(String name , String date , String location, int serviceNum, int guestNum){
+public void createParty(String name , String date , String location, int serviceNum, int guestNum){ //creates a new party for the client
 party = new Party(name, date, location, serviceNum, guestNum);
 
 }
 
 
-public Party getParty(){
+public Party getParty(){ //returns the party associated with the client
 return party;
 }
 
 
-public void displayDetails(){
+public void displayDetails(){ //displays the client's information
 super.displayDetails();
+}
+
+public double calculateCost() { /*calculates the total cost of the party for the client by calling the 
+calculateTotalCost() method from the Party class and returns the result*/
+    return party.calculateTotalCost();
 }
 
 }
