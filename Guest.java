6c6402a336db64131f8abe7e6d4ccafc96fb7270
Aljawharah46 <aljbk4606@gmail.com>
2026-04-22 13:53:45 +0300

    Add files via upload

--- Guest.java
+++ Guest.java
@@ -2,12 +2,12 @@ public class Guest extends Person{
 
 private String invitationID;
 
-public Guest(String n , String phoneNum , String invitationID){
+public Guest(String n , String phoneNum , String invitationID){ //constructor
 super(n , phoneNum);
 this.invitationID=invitationID;
 }
 
-public void displayDetails(){
+public void displayDetails(){ //displays the guest's information
 super.displayDetails();
 System.out.println("Invitation ID: "+invitationID);
 }
