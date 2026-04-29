a8b2afb9e262592b707813068fa28ca5afe4762d
raghad1212 <raghadalistudent@gmail.com>
2026-04-29 16:36:35 +0300

    Update Client.java

--- Client.java
+++ Client.java
@@ -2,27 +2,26 @@ public class Client extends Person implements Payable {
 
 private Party party=null;
 
-public Client(String n , String phoneNum){ //constructor
+public Client(String n , String phoneNum){
 super(n , phoneNum);
 }
 
-public void createParty(String name , String date , String location, int serviceNum, int guestNum){ //creates a new party for the client
-party = new Party(name, date, location, serviceNum, guestNum);
+public void createParty(String name , String date , String location, int serviceNum, int guestNum)  {
+party = new Party(name, date, location, serviceNum, guestNum); 
 
 }
 
 
-public Party getParty(){ //returns the party associated with the client
+public Party getParty(){
 return party;
 }
 
 
-public void displayDetails(){ //displays the client's information
+public void displayDetails(){
 super.displayDetails();
 }
 
-public double calculateCost() { /*calculates the total cost of the party for the client by calling the 
-calculateTotalCost() method from the Party class and returns the result*/
+public double calculateCost() {
     return party.calculateTotalCost();
 }
 
